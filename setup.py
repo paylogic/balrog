fc4d246926d57f26d44fde649706a5416a919b74
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-09-24 10:22:21 +0200

    use tox instead of setup.py integration

--- setup.py
+++ setup.py
@@ -3,30 +3,11 @@
 
 import os
 import codecs
-import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 import balrog
 
-
-class ToxTestCommand(TestCommand):
-
-    """Test command which runs tox under the hood."""
-
-    def finalize_options(self):
-        """Add options to the test runner (tox)."""
-        TestCommand.finalize_options(self)
-        self.test_args = ['--recreate']
-        self.test_suite = True
-
-    def run_tests(self):
-        """Invoke the test runner (tox)."""
-        # import here, cause outside the eggs aren't loaded
-        import detox.main
-        errno = detox.main.main(self.test_args)
-        sys.exit(errno)
 
 long_description = []
 
@@ -56,7 +37,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3"
     ] + [("Programming Language :: Python :: %s" % x) for x in "2.6 2.7 3.4".split()],
-    cmdclass={"test": ToxTestCommand},
     packages=["balrog"],
     tests_require=["detox"],
 
